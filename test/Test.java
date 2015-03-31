c81d23ae4003398c50853f05f1e85fcb96fb31ad
MrColdBeverage <ndiamond96@gmail.com>
2015-03-31 02:56:54 -0700

    Added my name to the java file
    made it a GUI

--- test/Test.java
+++ test/Test.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+import java.awt.*;
 
 public class Test
 {
@@ -5,7 +7,27 @@ public class Test
 	{
 		System.out.println("Benson Chu");
 		System.out.println("Josh Patterson");
-		
 		System.out.println("Test");
+		new NoahClass();
+	}
+}
+
+class NoahClass extends JFrame
+{
+	public NoahClass()
+	{
+		setSize(200, 200);
+		setResizable(false);
+		setVisible(true);
+		setContentPane(new GUI());
+	}
+	class GUI extends JPanel
+	{
+		public GUI(){}
+		public void paintComponent(Graphics g)
+		{
+			g.setFont(new Font("Impact",Font.BOLD ,48 ));
+			g.drawString("Noah", 50, 50);
+		}
 	}
 }
