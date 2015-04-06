4b939314e30bd7f1a77637770fd910d533a83b89
Deathtoall457 <benson.chu@hotmail.com>
2015-04-06 14:20:30 -0500

    Seizure

--- test/Test.java
+++ test/Test.java
@@ -15,18 +15,42 @@ public class Test
 
 class NoahClass extends JFrame
 {
+	GUI g;
 	public NoahClass()
 	{
+		g = new GUI();
 		setSize(200, 200);
 		setResizable(false);
 		setVisible(true);
-		setContentPane(new GUI());
+		setContentPane(g);
+		
+		run();
 	}
+	
+	public void run()
+	{
+		while(true)
+		{
+			g.repaint();
+			try{
+				Thread.sleep(50);
+			}
+			catch(InterruptedException e){e.printStackTrace();}
+		}
+	}
+	
 	class GUI extends JPanel
 	{
 		public GUI(){}
 		public void paintComponent(Graphics g)
 		{
+			Color f = new Color((int)(Math.random() * 255),(int)(Math.random() * 255),(int)(Math.random() * 255));
+			Color b = new Color((int)(Math.random() * 255),(int)(Math.random() * 255),(int)(Math.random() * 255));
+			
+			g.setColor(b);
+			g.fillRect(0,0,200,200);
+			
+			g.setColor(f);
 			g.setFont(new Font("Impact",Font.BOLD ,48 ));
 			g.drawString("Noah", 50, 50);
 		}
